0715c8696713c31bb1f5ad747e60fe083b102075
Lauren Albert <pleaseonthekeys@gmail.com>
2020-04-17 19:05:05 -0400

    initializes namespace and interfaces for users, Memories, and Songs

---
+++ src/types.ts
@@ -0,0 +1,58 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+
+export namespace mb {
+  export interface User {
+    firstName: string;
+    lastName: string;
+    memberType: User.memberType;
+    address: string;
+    birthday: string;
+    createdBy: string;
+    email: string;
+    password: string;
+  }
+
+  export namespace User {
+    export type memberType = 'user' | 'family' | 'friend' | 'caretaker';
+
+    export interface UserDoc extends User {
+      _id: string;
+      createdAt: string;
+      updatedAt: string;
+    }
+  }
+
+  export interface Memory {
+    user: string;
+    song: string;
+    text: string;
+    tags: Array<string>;
+    createdBy: string;
+  }
+
+  export namespace Memory {
+    export interface MemoryDoc extends Memory {
+      _id: string;
+      createdAt: string;
+      updatedAt: string;
+    }
+  }
+
+  export interface Song {
+    title: string;
+    artist: string;
+    user: string;
+    year: number;
+    spotifyLink: string;
+    youTubeLink?: string;
+    favorite?: boolean;
+  }
+
+  export namespace Song {
+    export interface SongDoc extends Song {
+      _id: string;
+      createdAt: string;
+      updatedAt: string;
+    }
+  }
+}
