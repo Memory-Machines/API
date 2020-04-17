c69e8db4c101f8aec72d13330b701c72d8ba7a21
Lauren Albert <pleaseonthekeys@gmail.com>
2020-04-17 19:08:08 -0400

    includes body parser

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 import router from './routes';
 
 const app = express();
+//parse body
+app.use(bodyParser.json());
+//parse url
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', router);
 
 export default app;
