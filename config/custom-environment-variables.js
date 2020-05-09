1106477ab359b53290e00d930652d2ed098701cb
Lauren Albert <pleaseonthekeys@gmail.com>
2020-05-08 21:54:11 -0400

    only includes necessary parameters

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -1,12 +1,6 @@
 module.exports = {
   server: {
-    context: 'NODE_CONFIG_ENV',
     env: 'NODE_ENV',
     dbUri: 'DB_URI',
-  },
-  services: {
-    sendgrid: {
-      apiKey: 'SENDGRID_API_KEY',
-    },
   },
 };
--- config/default.js
+++ config/default.js
@@ -1,16 +1,7 @@
 module.exports = {
   server: {
-    context: 'development',
     env: 'development',
     port: 8080,
-    app: '',
-    admin: '',
     dbUri: '',
-    jwtSecret: '',
-  },
-  services: {
-    sendgrid: {
-      apiKey: '',
-    },
   },
 };
