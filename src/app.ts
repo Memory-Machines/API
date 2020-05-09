f8fe0249ef85a9c7686e4dbcadc64346690ac50d
Lauren Albert <pleaseonthekeys@gmail.com>
2020-05-08 21:51:14 -0400

    uses morgan

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
+//need to nom install or add a .d.ts file containing `declare module 'cookie-parser'`
+// import cookieParser from 'cookie-parser';
 
 import router from './routes';
 
@@ -8,6 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 //parse url
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 app.use('/api', router);
 
 export default app;
