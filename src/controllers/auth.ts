2d43ba9d3198e99801352d0e30432e73cf3b908e
Lauren Albert <pleaseonthekeys@gmail.com>
2020-05-22 23:19:04 -0400

    login controller uses session data

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,85 +1,75 @@
 import { Request, Response, NextFunction } from 'express';
-import querystring from 'querystring';
 
 import { Auth } from '../services';
-import { spotify_cred } from '../utils/credentials';
-import { generateRandomString } from '../utils/spotifyToken';
-
-async function registerUser(req: Request, res: Response, next: NextFunction) {
-  try {
-    const { firstName, lastName, memberType, address, birthday, createdBy, email, password } = req.body;
-
-    const user = await Auth.registerUser({
-      firstName,
-      lastName,
-      memberType,
-      address,
-      birthday,
-      createdBy,
-      email,
-      password,
-    });
-    res.send(user); //sending plain user object
-  } catch (e) {
-    console.error('AuthController::registerUser', e);
-    res.status(500).send(e.message);
-  }
-}
 
 async function login(req: Request, res: Response, next: NextFunction) {
   try {
-    const { email, password } = req.body;
-    const user = await Auth.login({
-      email,
-      password,
-    });
-    //generate cookie
-    const state = generateRandomString(16);
-    res.cookie(spotify_cred.stateKey, state);
-
-    // your application requests authorization
-    res.redirect(
-      'https://accounts.spotify.com/authorize?' +
-        querystring.stringify({
-          response_type: 'code',
-          client_id: spotify_cred.client_id,
-          scope: spotify_cred.scope,
-          redirect_uri: spotify_cred.redirect_uri,
-          state: state,
-        })
-    );
-    //Can I also res.send after redirecting? Do I even need to? Why am I sending this user object back anyways?
-    res.send(user); //sending plain user object
+    if (req.session.email) {
+      return res.send({ message: 'Welcome Home', user: req.session.user });
+    }
+    const { code } = req.query;
+    const token = await Auth.spotifyAuthTokenRequest(code);
+    const user = await Auth.getUserEmail(token.access_token);
+    // check user in the database here
+    const sess = req.session;
+    sess.email = user.email;
+    sess.tokenData = token;
+    sess.user = user;
+    res.send({ message: 'ok' });
   } catch (e) {
     console.error('AuthController::login', e);
     res.status(500).send(e.message);
   }
 }
 
-async function spotifyAuthCodeRequest(req: Request, res: Response, next: NextFunction) {
+async function authenticatedRoute(req: Request, res: Response, next: NextFunction) {
   try {
-    //here we are making a call to spotify/authorize...
-    const spotifyAuth = await Auth.spotifyAuthCodeRequest();
-    //how do I know if the user is actually given the url with the code? How do I test that in postman?
-    res.send(spotifyAuth.data);
+    if (req.session.email) {
+      // user is authenticated
+      return res.send({ message: 'Welcome Home', user: req.session.user });
+    } else {
+      return res.status(401).send({ message: 'Go Back' });
+    }
   } catch (e) {
-    console.error('AuthController::spotifyAuthCodeRequest', e);
+    console.error('AuthController::authenticatedRoute', e);
     res.status(500).send(e.message);
   }
 }
 
-async function spotifyAuthTokenRequest(req: Request, res: Response, next: NextFunction) {
-  const { code } = req.query;
-  console.log({ code });
-  //do something to make call to https://accounts.spotify.com/api/token
-
+async function logout(req: Request, res: Response, next: NextFunction) {
   try {
-    const token = await Auth.spotifyAuthTokenRequest(code);
-    res.send(token);
+    req.session.destroy((err) => {
+      if (err) {
+        return console.log(err);
+      }
+      res.redirect('/');
+    });
   } catch (e) {
-    console.error('AuthController::spotifyAuthTokenRequest', e);
+    console.error('AuthController::logout', e);
     res.status(500).send(e.message);
   }
 }
 
-export { registerUser, login, spotifyAuthCodeRequest, spotifyAuthTokenRequest };
+async function loginLink(req: Request, res: Response, next: NextFunction) {
+  try {
+    const my_client_id = '54bb3d7498da476987b8463ca50ff4df';
+    const redirect_uri = 'http://localhost:3000/redirect/login/spotify';
+    const scopes = 'streaming user-read-private user-read-email';
+    console.log(req.sessionID);
+    res.send(
+      'https://accounts.spotify.com/authorize' +
+        '?response_type=code' +
+        '&client_id=' +
+        my_client_id +
+        '&scope=' +
+        encodeURIComponent(scopes) +
+        '&redirect_uri=' +
+        encodeURIComponent(redirect_uri)
+    );
+  } catch (e) {
+    console.error('AuthController::loginLink', e);
+    res.status(500).send(e.message);
+  }
+}
+
+export { login, logout, authenticatedRoute, loginLink };
