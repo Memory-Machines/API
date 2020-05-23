ea505e0dcaf1078257eddab4e284d6b0d3c01ee1
Lauren Albert <pleaseonthekeys@gmail.com>
2020-05-22 23:20:40 -0400

    initializes getUserEmail service

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,54 +1,12 @@
 import bcrypt from 'bcrypt';
 import config from 'config';
 
-import { User } from '../services';
+import { User } from '.';
 import { mb } from '../types';
 import { getSpotifyToken, getSpotifyCode } from '../utils/spotifyToken';
+import axios from 'axios';
 
 const saltRounds = 10;
-
-async function registerUser(data: mb.User): Promise<mb.User.UserDoc> {
-  try {
-    const passwordHash = await bcrypt.hash(data.password, saltRounds);
-    const user: mb.User = {
-      firstName: data.firstName,
-      lastName: data.lastName,
-      memberType: data.memberType,
-      address: data.address,
-      birthday: data.birthday,
-      createdBy: data.createdBy,
-      email: data.email,
-      password: passwordHash,
-    };
-    const result = await User.createUser(user);
-    delete result.password;
-    return result;
-  } catch (e) {
-    console.error('authService::registerUSer', e);
-    throw e;
-  }
-}
-
-async function login(data: { email: string; password: string }) {
-  try {
-    //1. get from our collection, whether or not that email exists
-    //2. make sure password in that document matches correctly
-    //3. if it does, then log them in
-    //4. if not throw error
-    //In order to query a specific entity, we must go through that entity's specific service which is importing its repository which is importing its mongoose model
-    const result = await User.findUserWithEmail(data.email);
-    const isAMatch = await bcrypt.compare(data.password, result.password);
-    if (isAMatch) {
-      //actually return cookie/session info
-      delete result.password;
-      return result;
-    }
-    throw new Error('invalid email or password');
-  } catch (e) {
-    console.error('authService::login', e);
-    throw e;
-  }
-}
 
 async function spotifyAuthTokenRequest(code: string) {
   try {
@@ -67,14 +25,22 @@ async function spotifyAuthTokenRequest(code: string) {
   }
 }
 
-async function spotifyAuthCodeRequest() {
+async function getUserEmail(access_token: string) {
   try {
-    const data = await getSpotifyCode();
+    const headers = {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: 'Bearer ' + access_token,
+    };
+    const { data } = await axios.get('https://api.spotify.com/v1/me', {
+      headers,
+    });
+    console.log(data);
     return data;
   } catch (e) {
-    console.error('authService::spotifyAuthCodeRequest', e);
+    console.error('authService::spotifyAuthTokenRequest', e);
     throw e;
   }
 }
 
-export { registerUser, login, spotifyAuthTokenRequest, spotifyAuthCodeRequest };
+export { spotifyAuthTokenRequest, getUserEmail };
